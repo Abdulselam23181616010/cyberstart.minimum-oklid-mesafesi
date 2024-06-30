e88605fbb0337f8f14c724e16b9c8c47eb664a73
Abdülselam_İsmail <147073125+Abdulselam23181616010@users.noreply.github.com>
2024-06-30 14:26:13 +0300

    Update main.py
    got rid of errors

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-def euclideanDistance(tuple nokta1, tuple nokta2):
+def euclideanDistance(nokta1, nokta2):
   mesafe = ((nokta1[0]-nokta2[0])**2 + (nokta1[1]-nokta2[1])**2)**(1/2)
   return mesafe
-list1 = [(1,2),(2,5),(9,12))
+list1 = [(1,2),(2,5),(9,12)]
 mesafeler = []
-for i in len(list1):
+for i in range(len(list1)):
   j=i+1
   while j<len(list1):
-    mesafeler.append(euclideanDistance(list1[i],list[j])
-    j++
+    mesafeler.append(euclideanDistance(list1[i],list1[j]))
+    j+=1
 print(min(mesafeler))
     
             
+
